2abb3f4cb3042bde2d5c945e61967c6833f32d69
w356877795 <wangxiuqi@126.com>
2020-07-27 16:16:35 +0800

    delete all text in test1.c add 1111

--- test1.c
+++ test1.c
@@ -1,16 +1,1 @@
-
-master_test
-
-branch1_test add some text 22:34
- 
-branch2_test "add new text" 30300630 09:32 sda fds
-
-
-"add by wwx12:37"
-<<<<<<< HEAD
-
-"add by wwx12:45"
-
-=======
-"add by wwx12:43"
->>>>>>> 82f95a4f8f628a123646c99aa33d6c69d22ce7fa
+//111111111111111111111111111
